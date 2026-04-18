deafffdb9b16668511b1acc86756f44fffa17dfa
gashaw27 <gashawsitotaw632@gmail.com>
2026-04-17 20:56:24 -0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { inject } from '@vercel/analytics';
+inject();
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css' // <--- ይሄ መስመር መኖሩን በደንብ አረጋግጥ! ካሌለ ዲዛይኑ አይመጣም።
